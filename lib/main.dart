4e7ffd34028186d71d8589a130f633fe374819fa
amostafa015 <ahmed.g.mostafa@pwc.com>
2025-07-31 13:15:13 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,29 @@ import 'package:toktokapp/domain/usecases%20/send_otp_usecase.dart';
 import 'package:toktokapp/presentation/bloc/auth_bloc/auth_bloc.dart';
 import 'package:toktokapp/presentation/bloc/map_bloc.dart';
 import 'package:toktokapp/presentation/bloc/otp_bloc/otp_bloc.dart';
-import 'package:toktokapp/presentation/screens/map_screen.dart';
-import 'package:toktokapp/presentation/screens/phone_auth_screen.dart';
 import 'package:toktokapp/presentation/screens/splash_screen.dart';
 import 'package:toktokapp/controllers/theme_controller.dart';
 import 'package:toktokapp/translations/app_translation.dart';
+import 'package:toktokapp/presentation/screens/otp_screen.dart';
+import 'package:toktokapp/presentation/screens/phone_auth_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  initDependencies();
-  runApp(MyApp());
+  final firebaseAuth = FirebaseAuth.instance;
+  final dependencies = AppDependencies(firebaseAuth);
+  runApp(MyApp(
+    dependencies: dependencies
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  final AppDependencies dependencies;
+  MyApp({super.key, required this.dependencies});
+
+
 
   final ThemeController themeController = Get.put(ThemeController());
   final languageController = Get.put(LanguageController());
@@ -39,61 +47,91 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
-        return Obx(
-          () => MultiBlocProvider(
+        return MultiBlocProvider(
             providers: [
-              BlocProvider(create: (context) => Get.find<PhoneAuthBloc>()),
-              BlocProvider(
-                create: (context) => Get.find<OtpVerificationBloc>(),
-              ),
-              BlocProvider(
-                create: (context) => Get.find<MapBloc>(),
-              ),
+              BlocProvider<PhoneAuthBloc>.value(value: dependencies.phoneAuthBloc),
+              BlocProvider<OtpVerificationBloc>.value(value: dependencies.otpVerificationBloc),
+              BlocProvider<MapBloc>.value(value: dependencies.mapBloc),
             ],
-            child: GetMaterialApp(
-              debugShowCheckedModeBanner: false,
-              theme: AppThemes.lightTheme,
-              darkTheme: AppThemes.darkTheme,
-              themeMode: themeController.theme,
-              locale: Locale(languageController.currentLanguage.value),
-              textDirection:
-                  languageController.isEnglish
-                      ? TextDirection.ltr
-                      : TextDirection.ltr,
-              translations: AppTranslations(),
-              home: MapScreen(),
-            ),
-          ),
+            child: MultiRepositoryProvider(
+              providers: [
+                RepositoryProvider<VerifyOtpUseCase>.value(value: dependencies.verifyOtpUseCase),
+              ],
+              child: GetMaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: AppThemes.lightTheme,
+                darkTheme: AppThemes.darkTheme,
+                themeMode: themeController.theme,
+                locale: Locale(languageController.currentLanguage.value),
+                textDirection:
+                languageController.isEnglish
+                    ? TextDirection.ltr
+                    : TextDirection.ltr,
+                // translations: AppTranslations(),
+                // localizationsDelegates: AppLocalizations.localizationsDelegates,
+                // supportedLocales: AppLocalizations.supportedLocales,
+                home: SplashScreen(),
+              ),
+            )
         );
       },
     );
   }
 }
 
-void initDependencies() {
-  Get.lazyPut<PhoneAuthRemoteDataSource>(
-    () => PhoneAuthRemoteDataSourceImpl(FirebaseAuth.instance),
-  );
+class AppDependencies {
+  final FirebaseAuth firebaseAuth;
 
-  Get.lazyPut<PhoneAuthRepository>(
-    () => PhoneAuthRepositoryImpl(Get.find<PhoneAuthRemoteDataSource>()),
-  );
+  late final PhoneAuthRemoteDataSource phoneAuthRemoteDataSource;
+  late final PhoneAuthRepository phoneAuthRepository;
+  late final SendOtpUseCase sendOtpUseCase;
+  late final PhoneAuthBloc phoneAuthBloc;
 
-  Get.lazyPut(() => SendOtpUseCase(Get.find<PhoneAuthRepository>()));
+  late final OtpVerificationRemoteDataSource otpVerificationRemoteDataSource;
+  late final OtpVerificationRepository otpVerificationRepository;
+  late final VerifyOtpUseCase verifyOtpUseCase;
+  late final OtpVerificationBloc otpVerificationBloc;
 
-  Get.lazyPut(() => PhoneAuthBloc(Get.find<SendOtpUseCase>()));
+  late final MapBloc mapBloc;
 
-  Get.lazyPut<OtpVerificationRemoteDataSource>(
-    () => OtpVerificationRemoteDataSourceImpl(FirebaseAuth.instance),
-  );
-
-  Get.lazyPut<OtpVerificationRepository>(
-    () => OtpVerificationRepositoryImpl(
-      Get.find<OtpVerificationRemoteDataSource>(),
-    ),
-  );
-
-  Get.lazyPut(() => VerifyOtpUseCase(Get.find<OtpVerificationRepository>()));
-
-  Get.lazyPut(() => MapBloc());
+  AppDependencies(this.firebaseAuth) {
+    phoneAuthRemoteDataSource = PhoneAuthRemoteDataSourceImpl(firebaseAuth);
+    phoneAuthRepository = PhoneAuthRepositoryImpl(phoneAuthRemoteDataSource);
+    sendOtpUseCase = SendOtpUseCase(phoneAuthRepository);
+    phoneAuthBloc = PhoneAuthBloc(sendOtpUseCase);
+    otpVerificationRemoteDataSource = OtpVerificationRemoteDataSourceImpl(firebaseAuth);
+    otpVerificationRepository = OtpVerificationRepositoryImpl(otpVerificationRemoteDataSource);
+    verifyOtpUseCase = VerifyOtpUseCase(otpVerificationRepository);
+    otpVerificationBloc = OtpVerificationBloc(verifyOtpUseCase: verifyOtpUseCase,verificationId: "");
+    mapBloc = MapBloc();
+  }
 }
+
+// void initDependencies() {
+//   Get.lazyPut<PhoneAuthRemoteDataSource>(
+//         () => PhoneAuthRemoteDataSourceImpl(FirebaseAuth.instance),
+//   );
+//
+//   Get.lazyPut<PhoneAuthRepository>(
+//         () => PhoneAuthRepositoryImpl(Get.find<PhoneAuthRemoteDataSource>()),
+//   );
+//
+//   Get.lazyPut(() => SendOtpUseCase(Get.find<PhoneAuthRepository>()));
+//
+//   Get.lazyPut(() => PhoneAuthBloc(Get.find<SendOtpUseCase>()));
+//
+//   Get.lazyPut<OtpVerificationRemoteDataSource>(
+//         () => OtpVerificationRemoteDataSourceImpl(FirebaseAuth.instance),
+//   );
+//
+//   Get.lazyPut<OtpVerificationRepository>(
+//         () =>
+//         OtpVerificationRepositoryImpl(
+//           Get.find<OtpVerificationRemoteDataSource>(),
+//         ),
+//   );
+//
+//   Get.lazyPut(() => VerifyOtpUseCase(Get.find<OtpVerificationRepository>()));
+//
+//   Get.lazyPut(() => MapBloc());
+// }
